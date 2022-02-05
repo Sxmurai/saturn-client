d43e2362d0c08dd121d6ec8d00cfabcc102a248f
Sxmurai <gavrw0607@gmail.com>
2022-02-04 20:34:16 -0600

    (impl) add button click sound weee

--- src/main/java/cope/saturn/core/ui/click/components/impl/Button.java
+++ src/main/java/cope/saturn/core/ui/click/components/impl/Button.java
@@ -6,6 +6,8 @@
 package cope.saturn.core.ui.click.components.impl;
 
 import cope.saturn.core.ui.click.components.Component;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.sound.SoundEvents;
 
 public abstract class Button extends Component {
     public Button(String name) {
@@ -18,6 +20,7 @@ public abstract class Button extends Component {
     public void mouseClicked(double mouseX, double mouseY, int button) {
         if (isMouseInBounds(mouseX, mouseY)) {
             onInteract(button);
+            mc.getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_BUTTON_CLICK, 1.0f));
         }
     }
 }
