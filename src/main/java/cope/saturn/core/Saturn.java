e3936ada46609ff75537fc11914bc42836160e7f
Sxmurai <gavrw0607@gmail.com>
2022-02-16 23:07:41 -0600

    (impl) Version should now be on beta channel

--- src/main/java/cope/saturn/core/Saturn.java
+++ src/main/java/cope/saturn/core/Saturn.java
@@ -18,7 +18,7 @@ public class Saturn implements ClientModInitializer {
     public static Saturn INSTANCE;
 
     public static final String NAME = "Saturn";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0-beta";
 
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
 
